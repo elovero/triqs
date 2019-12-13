437f0d68bbb903eb4fcd9051451fa73c8b826522
lizlove <elizabethlovero@gmail.com>
2019-12-13 09:58:58 -0500

    Font scaling.

--- doc/conf.py
+++ doc/conf.py
@@ -90,7 +90,7 @@ html_short_title = "TRIQS"
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = './_static/logo_x.png'
+# html_logo = './_static/triqs_logo_sample.jpg'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
